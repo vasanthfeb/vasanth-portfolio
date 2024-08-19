90cd9f29eb85ddd5c1c48c9b806387eafad5ed89
vasanthfeb <vasanthfeb18@gmail.com>
2024-08-19 20:45:42 +0530

    vasanth-portfolio master

--- script.js
+++ script.js
@@ -101,37 +101,37 @@ const Type = async (words, elementId) => {
     
     $(document).ready(function(){
     $(".all1").click(function(){
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".clg1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".bus1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".git1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".hos1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".exp1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".clg1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".bus1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".git1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".hos1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".exp1").css({"background-color": "#000000", "color": "#FF0000"});
     
     });
     
     
     $(".exp1").click(function(){
-    $(".exp1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".clg1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".bus1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".git1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".hos1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".all1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".exp1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".clg1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".bus1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".git1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".hos1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".all1").css({"background-color": "#000000", "color": "#FF0000"});
     });
     
     
     
     
     $(".hos1").click(function(){
-    $(".hos1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".clg1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".bus1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".git1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".all1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".exp1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".hos1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".clg1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".bus1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".git1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".all1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".exp1").css({"background-color": "#000000", "color": "#FF0000"});
     });
     
     
@@ -139,38 +139,38 @@ const Type = async (words, elementId) => {
     
     
     $(".bus1").click(function(){
-    $(".bus1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".clg1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".all1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".git1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".hos1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".exp1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".bus1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".clg1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".all1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".git1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".hos1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".exp1").css({"background-color": "#000000", "color": "#FF0000"});
     });
     
     
     
     $(".clg1").click(function(){
-    $(".clg1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".bus1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".git1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".hos1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".exp1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".clg1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".bus1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".git1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".hos1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".exp1").css({"background-color": "#000000", "color": "#FF0000"});
     });
     
     
     
     
     $(".git1").click(function(){
-    $(".git1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".all1").css({"background-color": "#08b1ff", "color": "black"});
-    $(".clg1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".bus1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".all1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".hos1").css({"background-color": "#000000", "color": "#08b1ff"});
-    $(".exp1").css({"background-color": "#000000", "color": "#08b1ff"});
+    $(".git1").css({"background-color": "#FF0000", "color": "black"});
+    $(".all1").css({"background-color": "#FF0000", "color": "black"});
+    $(".clg1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".bus1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".all1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".hos1").css({"background-color": "#000000", "color": "#FF0000"});
+    $(".exp1").css({"background-color": "#000000", "color": "#FF0000"});
     });
     
     
